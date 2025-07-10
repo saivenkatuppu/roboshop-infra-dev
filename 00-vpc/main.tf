48e1ff73c497cbf79cb922c3b309b502b81f7349
sai <saivenkat67687@gmail.com>
2025-07-10 22:47:14 +0530

    terraform

--- 00-vpc/main.tf
+++ 00-vpc/main.tf
@@ -1,11 +1,12 @@
 module "vpc" {
      #source = "../terraform-aws-vpc"
-    source = "git::https://github.com/saivenkatuppu/terraform-aws-vpc.git?ref=main"  # refering from git repository
+      source = "git::https://github.com/saivenkatuppu/terraform-aws-vpc.git?ref=main"  # refering from git repository
+      # source =  "git::https://github.com/SurekhaBandaru/terraform-aws-vpc.git?ref=main"
     project = var.project
     environment = var.environment
     public_subnet_cidr = var.public_subnet_cidr
     private_subnet_cidr = var.private_subnet_cidr
     database_subnet_cidr = var.database_subnet_cidr
 
-    is_peering_required = true
+     is_peering_required = true
 }
